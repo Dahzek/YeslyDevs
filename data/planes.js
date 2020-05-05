3ab728716593b0790c95fac655b1d8837081b6f8
akuseryuinc <36928833+Dahzek@users.noreply.github.com>
2020-05-04 21:18:49 -0400

    Create planes.js

---
+++ data/planes.js
@@ -0,0 +1,251 @@
+{
+  "planes": [
+    {
+      "nombre": "plan_basico",
+      "data": {
+        "secciones": 4,
+        "enlaces_rrss": {
+          "tiene": true,
+          "tipo": "normal"
+        },
+        "formularios": 1,
+        "dominio_cl": 10000,
+        "hosting": "basico",
+        "ancho_banda": "ilimitado",
+        "hosting_archivos_dedicado": 40,
+        "correos_corporativos": {
+          "hasta": 2,
+          "espacio_cuenta": 512
+        },
+        "base_datos": 1,
+        "cloudflare": true,
+        "cert_ssl": {
+          "tiene": false,
+          "bit": 0
+        },
+        "copias_seguridad": {
+          "tiene": false,
+          "frecuencia": "0"
+        },
+        "datos_estadisticas": "simple",
+        "optimizacion_seo": true,
+        "iconos": "basico",
+        "soporte": "basico",
+        "administrador_correos": true,
+        "panel_control": true,
+        "file_uploader": true,
+        "generador_pw": true,
+        "publicador_contenido": false,
+        "acortador_url": false,
+        "seguimiento_estatus": false,
+        "precopiado": false,
+        "administrador_tareas": false,
+        "contenidos_rrss": {
+          "tiene": true,
+          "publicidad": true
+        },
+        "sistema_referidos": true
+      }
+    },
+    {
+      "nombre": "plan_galeria",
+      "data": {
+        "secciones": 6,
+        "enlaces_rrss": {
+          "tiene": true,
+          "tipo": "configurables"
+        },
+        "formularios": 3,
+        "dominio_cl": 10000,
+        "hosting": "premium",
+        "ancho_banda": "ilimitado",
+        "hosting_archivos_dedicado": 200,
+        "correos_corporativos": {
+          "hasta": 4,
+          "espacio_gb": 2
+        },
+        "base_datos": 1,
+        "cloudflare": {
+          "tiene": true,
+          "precio_plan": "gratis"
+        },
+        "cert_ssl": {
+          "tiene": false,
+          "bit": 2048
+        },
+        "copias_seguridad": {
+          "tiene": false,
+          "frecuencia": "semanales"
+        },
+        "datos_estadisticas": "avanzado",
+        "optimizacion_seo": true,
+        "iconos": "avanzado",
+        "soporte": "premium",
+        "administrador_correos": true,
+        "panel_control": true,
+        "image_uploader": true,
+        "generador_pw": true,
+        "publicador_contenido": true,
+        "acortador_url": true,
+        "seguimiento_estatus": true,
+        "precopiado": true,
+        "administrador_tareas": true,
+        "contenidos_rrss": {
+          "tiene": true,
+          "publicidad": false
+        },
+        "sistema_referidos": true
+      }
+    },
+    {
+      "nombre": "plan_blog",
+      "data": {
+        "secciones": 8,
+        "enlaces_rrss": {
+          "tiene": true,
+          "tipo": "configurables"
+        },
+        "formularios": 3,
+        "dominio_cl": 10000,
+        "hosting": "premium",
+        "ancho_banda": "ilimitado",
+        "hosting_archivos_dedicado": 125,
+        "correos_corporativos": {
+          "hasta": 6,
+          "espacio_gb": 3
+        },
+        "base_datos": 1,
+        "cloudflare": {
+          "tiene": true,
+          "precio_plan": "gratis"
+        },
+        "cert_ssl": {
+          "tiene": false,
+          "bit": 2048
+        },
+        "copias_seguridad": {
+          "tiene": false,
+          "frecuencia": "semanales"
+        },
+        "datos_estadisticas": "avanzado",
+        "optimizacion_seo": true,
+        "iconos": "avanzado",
+        "soporte": "premium",
+        "administrador_correos": true,
+        "panel_control": true,
+        "image_uploader": true,
+        "generador_pw": true,
+        "publicador_contenido": true,
+        "acortador_url": true,
+        "seguimiento_estatus": true,
+        "precopiado": true,
+        "administrador_tareas": true,
+        "contenidos_rrss": {
+          "tiene": true,
+          "publicidad": false
+        },
+        "sistema_referidos": true
+      }
+    },
+    {
+      "nombre": "plan_tienda",
+      "data": {
+        "secciones": 10,
+        "enlaces_rrss": {
+          "tiene": true,
+          "tipo": "configurables"
+        },
+        "formularios": 6,
+        "dominio_cl": 10000,
+        "hosting": "dedicado",
+        "ancho_banda": "ilimitado",
+        "hosting_archivos_dedicado": 200,
+        "correos_corporativos": {
+          "hasta": 10,
+          "espacio_gb": 5
+        },
+        "base_datos": 1,
+        "cloudflare": {
+          "tiene": true,
+          "precio_plan": "gratis"
+        },
+        "cert_ssl": {
+          "tiene": false,
+          "bit": 2048
+        },
+        "copias_seguridad": {
+          "tiene": false,
+          "frecuencia": "semanales"
+        },
+        "datos_estadisticas": "avanzado",
+        "optimizacion_seo": true,
+        "iconos": "premium",
+        "soporte": "pro",
+        "administrador_correos": true,
+        "panel_control": true,
+        "image_uploader": true,
+        "generador_pw": true,
+        "publicador_contenido": true,
+        "acortador_url": true,
+        "seguimiento_estatus": true,
+        "precopiado": true,
+        "administrador_tareas": true,
+        "contenidos_rrss": {
+          "tiene": true,
+          "publicidad": false
+        },
+        "sistema_referidos": true
+      }
+    },
+    {
+      "nombre": "plan_pro",
+      "data": {
+        "secciones": 20,
+        "enlaces_rrss": {
+          "tiene": true,
+          "tipo": "configurables",
+          "formularios": 10
+        },
+        "dominio_cl": 10000,
+        "hosting": "dedicado",
+        "ancho_banda": "ilimitado",
+        "hosting_archivos_dedicado": 1000,
+        "correos_corporativos": {
+          "hasta": 50,
+          "espacio_gb": 25
+        },
+        "base_datos": true,
+        "cloudflare": {
+          "tiene": true,
+          "precio_plan": "gratis"
+        },
+        "cert_ssl": {
+          "tiene": false,
+          "bit": 4096
+        },
+        "copias_seguridad": {
+          "tiene": false,
+          "frecuencia": "diarias"
+        },
+        "datos_estadisticas": "avanzado",
+        "optimizacion_seo": true,
+        "iconos": "premium",
+        "soporte": "pro",
+        "administrador_correos": true,
+        "panel_control": true,
+        "image_uploader": true,
+        "generador_pw": true,
+        "publicador_contenido": true,
+        "acortador_url": true,
+        "seguimiento_estatus": true,
+        "precopiado": true,
+        "administrador_tareas": true,
+        "contenidos_rrss": {
+          "tiene": true,
+          "publicidad": false
+        },
+        "sistema_referidos": true
+      }
+    }
+  ]
+}
